843a9896eb022f4a8e674d0d57d2be153ee855df
Max <maxme1@users.noreply.github.com>
2023-01-21 08:55:37 +0300

    Delayed numpy import during setup (#33)
    * delayed numpy import during setup
    
    * indentation with 2 spaces instead of 4, as in original file

--- setup.py
+++ setup.py
@@ -2,32 +2,39 @@
 import setuptools
 import sys
 
-import numpy as np
+
+class NumpyImport:
+  def __repr__(self):
+    import numpy as np
+
+    return np.get_include()
+
+  __fspath__ = __repr__
+
 
 # NOTE: If fastremap.cpp does not exist, you must run
 # cython -3 --cplus fastremap.pyx
 
 extra_compile_args = [
-  '-std=c++11', '-O3', 
+  '-std=c++11', '-O3',
 ]
 
 if sys.platform == 'darwin':
-  extra_compile_args += [ '-stdlib=libc++', '-mmacosx-version-min=10.9' ]
+  extra_compile_args += ['-stdlib=libc++', '-mmacosx-version-min=10.9']
 
 setuptools.setup(
   setup_requires=['pbr', 'numpy'],
-  python_requires="~=3.6", # >= 3.6 < 4.0
+  python_requires="~=3.6",  # >= 3.6 < 4.0
   pbr=True,
   ext_modules=[
     setuptools.Extension(
       'fastremap',
-      sources=[ 'fastremap.cpp' ],
+      sources=['fastremap.cpp'],
       depends=[],
       language='c++',
-      include_dirs=[ np.get_include() ],
+      include_dirs=[NumpyImport()],
       extra_compile_args=extra_compile_args,
-    ) 
+    )
   ],
   long_description_content_type='text/markdown',
 )
-
