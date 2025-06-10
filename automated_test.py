468bc5e77af0d18280c4adaaa102759083cc739d
Matthew Vine <mattvine03@gmail.com>
2025-06-09 20:42:58 -0400

    chore: update tests

--- automated_test.py
+++ automated_test.py
@@ -302,94 +302,94 @@ def test_ascontiguousarray(dtype, dim):
 
 @pytest.mark.parametrize("dtype", [ np.uint8, np.uint16, np.uint32, np.uint64 ])
 def test_fit_dtype_uint(dtype):
-  assert fastremap.fit_dtype(dtype, 0) == np.uint8
-  assert fastremap.fit_dtype(dtype, 255) == np.uint8
-  assert fastremap.fit_dtype(dtype, 256) == np.uint16
-  assert fastremap.fit_dtype(dtype, 10000) == np.uint16
-  assert fastremap.fit_dtype(dtype, 2**16 - 1) == np.uint16
-  assert fastremap.fit_dtype(dtype, 2**16) == np.uint32
-  assert fastremap.fit_dtype(dtype, 2**32) == np.uint64
-  assert fastremap.fit_dtype(dtype, 2**64 - 1) == np.uint64
+  assert fastremap.fastremap.fit_dtype(dtype, 0) == np.uint8
+  assert fastremap.fastremap.fit_dtype(dtype, 255) == np.uint8
+  assert fastremap.fastremap.fit_dtype(dtype, 256) == np.uint16
+  assert fastremap.fastremap.fit_dtype(dtype, 10000) == np.uint16
+  assert fastremap.fastremap.fit_dtype(dtype, 2**16 - 1) == np.uint16
+  assert fastremap.fastremap.fit_dtype(dtype, 2**16) == np.uint32
+  assert fastremap.fastremap.fit_dtype(dtype, 2**32) == np.uint64
+  assert fastremap.fastremap.fit_dtype(dtype, 2**64 - 1) == np.uint64
 
   try:
-    fastremap.fit_dtype(dtype, -1)
+    fastremap.fastremap.fit_dtype(dtype, -1)
     assert False 
   except ValueError:
     pass
 
   try:
-    fastremap.fit_dtype(dtype, 2**64)
+    fastremap.fastremap.fit_dtype(dtype, 2**64)
   except ValueError:
     pass
 
 @pytest.mark.parametrize("dtype", [ np.int8, np.int16, np.int32, np.int64 ])
 def test_fit_dtype_int(dtype):
-  assert fastremap.fit_dtype(dtype, 0) == np.int8
-  assert fastremap.fit_dtype(dtype, 127) == np.int8
-  assert fastremap.fit_dtype(dtype, -128) == np.int8
-  assert fastremap.fit_dtype(dtype, 128) == np.int16
-  assert fastremap.fit_dtype(dtype, 10000) == np.int16
-  assert fastremap.fit_dtype(dtype, 2**15 - 1) == np.int16
-  assert fastremap.fit_dtype(dtype, 2**15) == np.int32
-  assert fastremap.fit_dtype(dtype, 2**32) == np.int64
-  assert fastremap.fit_dtype(dtype, 2**63 - 1) == np.int64
+  assert fastremap.fastremap.fit_dtype(dtype, 0) == np.int8
+  assert fastremap.fastremap.fit_dtype(dtype, 127) == np.int8
+  assert fastremap.fastremap.fit_dtype(dtype, -128) == np.int8
+  assert fastremap.fastremap.fit_dtype(dtype, 128) == np.int16
+  assert fastremap.fastremap.fit_dtype(dtype, 10000) == np.int16
+  assert fastremap.fastremap.fit_dtype(dtype, 2**15 - 1) == np.int16
+  assert fastremap.fastremap.fit_dtype(dtype, 2**15) == np.int32
+  assert fastremap.fastremap.fit_dtype(dtype, 2**32) == np.int64
+  assert fastremap.fastremap.fit_dtype(dtype, 2**63 - 1) == np.int64
 
   try:
-    fastremap.fit_dtype(dtype, 2**63)
+    fastremap.fastremap.fit_dtype(dtype, 2**63)
   except ValueError:
     pass
 
   try:
-    fastremap.fit_dtype(dtype, -2**63)
+    fastremap.fastremap.fit_dtype(dtype, -2**63)
   except ValueError:
     pass
 
 @pytest.mark.parametrize("dtype", [ np.float16, np.float32, np.float64 ])
 def test_fit_dtype_float(dtype):
-  assert fastremap.fit_dtype(dtype, 0) == np.float32
-  assert fastremap.fit_dtype(dtype, 127) == np.float32
-  assert fastremap.fit_dtype(dtype, 128) == np.float32
-  assert fastremap.fit_dtype(dtype, 10000) == np.float32
-  assert fastremap.fit_dtype(dtype, 2**15 - 1) == np.float32
-  assert fastremap.fit_dtype(dtype, 2**15) == np.float32
-  assert fastremap.fit_dtype(dtype, 2**32) == np.float32
-  assert fastremap.fit_dtype(dtype, 2**63 - 1) == np.float32
-  assert fastremap.fit_dtype(dtype, -2**63) == np.float32
-  assert fastremap.fit_dtype(dtype, 2**128) == np.float64
+  assert fastremap.fastremap.fit_dtype(dtype, 0) == np.float32
+  assert fastremap.fastremap.fit_dtype(dtype, 127) == np.float32
+  assert fastremap.fastremap.fit_dtype(dtype, 128) == np.float32
+  assert fastremap.fastremap.fit_dtype(dtype, 10000) == np.float32
+  assert fastremap.fastremap.fit_dtype(dtype, 2**15 - 1) == np.float32
+  assert fastremap.fastremap.fit_dtype(dtype, 2**15) == np.float32
+  assert fastremap.fastremap.fit_dtype(dtype, 2**32) == np.float32
+  assert fastremap.fastremap.fit_dtype(dtype, 2**63 - 1) == np.float32
+  assert fastremap.fastremap.fit_dtype(dtype, -2**63) == np.float32
+  assert fastremap.fastremap.fit_dtype(dtype, 2**128) == np.float64
 
-  assert fastremap.fit_dtype(dtype, 0, exotics=True) == np.float16
-  assert fastremap.fit_dtype(dtype, 127, exotics=True) == np.float16
-  assert fastremap.fit_dtype(dtype, 128, exotics=True) == np.float16
-  assert fastremap.fit_dtype(dtype, 10000, exotics=True) == np.float16
-  assert fastremap.fit_dtype(dtype, 2**15 - 1, exotics=True) == np.float16
-  assert fastremap.fit_dtype(dtype, 2**15, exotics=True) == np.float16
-  assert fastremap.fit_dtype(dtype, 2**32, exotics=True) == np.float32
-  assert fastremap.fit_dtype(dtype, 2**63 - 1, exotics=True) == np.float32
-  assert fastremap.fit_dtype(dtype, -2**63, exotics=True) == np.float32
+  assert fastremap.fastremap.fit_dtype(dtype, 0, exotics=True) == np.float16
+  assert fastremap.fastremap.fit_dtype(dtype, 127, exotics=True) == np.float16
+  assert fastremap.fastremap.fit_dtype(dtype, 128, exotics=True) == np.float16
+  assert fastremap.fastremap.fit_dtype(dtype, 10000, exotics=True) == np.float16
+  assert fastremap.fastremap.fit_dtype(dtype, 2**15 - 1, exotics=True) == np.float16
+  assert fastremap.fastremap.fit_dtype(dtype, 2**15, exotics=True) == np.float16
+  assert fastremap.fastremap.fit_dtype(dtype, 2**32, exotics=True) == np.float32
+  assert fastremap.fastremap.fit_dtype(dtype, 2**63 - 1, exotics=True) == np.float32
+  assert fastremap.fastremap.fit_dtype(dtype, -2**63, exotics=True) == np.float32
 
 @pytest.mark.parametrize("dtype", [ np.csingle, np.cdouble ])
 @pytest.mark.parametrize("sign", [ 1, -1, 1j, -1j ])
 def test_fit_dtype_float(dtype, sign):
-  assert fastremap.fit_dtype(dtype, sign * 0+0j) == np.csingle
-  assert fastremap.fit_dtype(dtype, sign * 127) == np.csingle
-  assert fastremap.fit_dtype(dtype, sign * 127) == np.csingle
-  assert fastremap.fit_dtype(dtype, sign * 128) == np.csingle
-  assert fastremap.fit_dtype(dtype, sign * 128) == np.csingle
-  assert fastremap.fit_dtype(dtype, sign * 10000) == np.csingle
-  assert fastremap.fit_dtype(dtype, sign * 10000) == np.csingle
-  assert fastremap.fit_dtype(dtype, sign * 2**15 - 1) == np.csingle
-  assert fastremap.fit_dtype(dtype, sign * 2**15) == np.csingle
-  assert fastremap.fit_dtype(dtype, sign * 2**32) == np.csingle
-  assert fastremap.fit_dtype(dtype, sign * 2**63 - 1) == np.csingle
-  assert fastremap.fit_dtype(dtype, -2**63) == np.csingle
+  assert fastremap.fastremap.fit_dtype(dtype, sign * 0+0j) == np.csingle
+  assert fastremap.fastremap.fit_dtype(dtype, sign * 127) == np.csingle
+  assert fastremap.fastremap.fit_dtype(dtype, sign * 127) == np.csingle
+  assert fastremap.fastremap.fit_dtype(dtype, sign * 128) == np.csingle
+  assert fastremap.fastremap.fit_dtype(dtype, sign * 128) == np.csingle
+  assert fastremap.fastremap.fit_dtype(dtype, sign * 10000) == np.csingle
+  assert fastremap.fastremap.fit_dtype(dtype, sign * 10000) == np.csingle
+  assert fastremap.fastremap.fit_dtype(dtype, sign * 2**15 - 1) == np.csingle
+  assert fastremap.fastremap.fit_dtype(dtype, sign * 2**15) == np.csingle
+  assert fastremap.fastremap.fit_dtype(dtype, sign * 2**32) == np.csingle
+  assert fastremap.fastremap.fit_dtype(dtype, sign * 2**63 - 1) == np.csingle
+  assert fastremap.fastremap.fit_dtype(dtype, -2**63) == np.csingle
   
   try:
-    fastremap.fit_dtype(dtype, sign * 2**128)
+    fastremap.fastremap.fit_dtype(dtype, sign * 2**128)
     assert False
   except ValueError:
     pass
 
-  assert fastremap.fit_dtype(dtype, sign * 2**128, exotics=True) == np.cdouble
+  assert fastremap.fastremap.fit_dtype(dtype, sign * 2**128, exotics=True) == np.cdouble
 
 def test_minmax():
   volume = np.random.randint(-500, 500, size=(128,128,128))
@@ -440,7 +440,7 @@ def test_unique(order):
 
   labels = reorder(np.random.randint(0, 500, size=(128,128,128)))
   uniq_np, idx_np, inv_np, cts_np = np.unique(labels, return_counts=True, return_index=True, return_inverse=True)
-  uniq_fr, idx_fr, cts_fr, inv_fr = fastremap.unique_via_array(labels.flatten(), np.max(labels), return_index=True, return_inverse=True)
+  uniq_fr, idx_fr, cts_fr, inv_fr = fastremap.fastremap.unique_via_array(labels.flatten(), np.max(labels), return_index=True, return_inverse=True)
   assert np.all(uniq_np == uniq_fr)
   assert np.all(inv_np.flatten() == inv_fr)
   assert np.all(cts_np == cts_fr)
@@ -457,7 +457,7 @@ def test_unique(order):
 
   labels = reorder(np.random.randint(-500, 500, size=(128,128,128)))
   uniq_np, idx_np, inv_np, cts_np = np.unique(labels, return_counts=True, return_index=True, return_inverse=True)
-  uniq_fr, idx_fr, cts_fr, inv_fr = fastremap.unique_via_shifted_array(labels.flatten(), return_index=True, return_inverse=True)
+  uniq_fr, idx_fr, cts_fr, inv_fr = fastremap.fastremap.unique_via_shifted_array(labels.flatten(), return_index=True, return_inverse=True)
   assert np.all(uniq_np == uniq_fr)
   assert np.all(inv_np.flatten() == inv_fr)
   assert np.all(cts_np == cts_fr)
@@ -475,7 +475,7 @@ def test_unique(order):
   # array_unique + shift
   labels = reorder(np.random.randint(128**3 - 500, 128**3 + 500, size=(128,128,128)))
   uniq_np, idx_np, inv_np, cts_np = np.unique(labels, return_counts=True, return_index=True, return_inverse=True)
-  uniq_fr, idx_fr, cts_fr, inv_fr = fastremap.unique_via_shifted_array(labels.flatten(), return_index=True, return_inverse=True)
+  uniq_fr, idx_fr, cts_fr, inv_fr = fastremap.fastremap.unique_via_shifted_array(labels.flatten(), return_index=True, return_inverse=True)
   assert np.all(uniq_np == uniq_fr)
   assert np.all(inv_np.flatten() == inv_fr)
   assert np.all(cts_np == cts_fr)
@@ -494,7 +494,7 @@ def test_unique(order):
   labels = reorder(np.random.randint(0, 1, size=(128,128,128)))
   labels[0,0,0] = 128**3 + 10
   uniq_np, idx_np, inv_np, cts_np = np.unique(labels, return_counts=True, return_index=True, return_inverse=True)
-  uniq_fr, idx_fr, cts_fr, inv_fr = fastremap.unique_via_renumber(labels.flatten(), return_index=True, return_inverse=True)
+  uniq_fr, idx_fr, cts_fr, inv_fr = fastremap.fastremap.unique_via_renumber(labels.flatten(), return_index=True, return_inverse=True)
   assert np.all(uniq_np == uniq_fr)
   assert np.all(inv_np.flatten() == inv_fr)
   assert np.all(cts_np == cts_fr)  
@@ -511,7 +511,7 @@ def test_unique(order):
 
   labels = reorder(np.random.randint(-1000, 128**3, size=(100,100,100)))
   uniq_np, cts_np = np.unique(labels, return_counts=True)
-  uniq_fr, cts_fr = fastremap.unique_via_sort(labels.flatten())
+  uniq_fr, cts_fr = fastremap.fastremap.unique_via_sort(labels.flatten())
   assert np.all(uniq_np == uniq_fr)
   assert np.all(cts_np == cts_fr)
 
