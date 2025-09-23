5774bfe44298ae94d58090ac841efd67582a1cb2
William Silversmith <william.silversmith@gmail.com>
2025-09-23 16:58:31 -0400

    docs: use more natural cython binding for remap, renumber for help

--- fastremap/fastremap.pyx
+++ fastremap/fastremap.pyx
@@ -118,10 +118,9 @@ def indices(cnp.ndarray[NUMBER, cast=True, ndim=1] arr, NUMBER value):
 
   return np.asarray(all_indices, dtype=np.uint64)
 
+@cython.binding(True)
 def renumber(arr, start=1, preserve_zero=True, in_place=False):
   """
-  renumber(arr, start=1, preserve_zero=True, in_place=False)
-
   Given an array of integers, renumber all the unique values starting
   from 1. This can allow us to reduce the size of the data width required
   to represent it.
@@ -652,11 +651,9 @@ def _inverse_component_map(
 
   return remap
 
+@cython.binding(True)
 def remap(arr, table, preserve_missing_labels=False, in_place=False):
   """
-  remap(cnp.ndarray[COMPLEX_NUMBER] arr, dict table, 
-    preserve_missing_labels=False, in_place=False)
-
   Remap an input numpy array in-place according to the values in the given 
   dictionary "table".   
 
